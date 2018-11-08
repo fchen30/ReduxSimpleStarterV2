7b85caf567968581a3cd9b204bf50e5c58e77f05
Fan Chen <chenfa@amazon.com>
2018-11-07 23:14:18 -0800

    update file

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        {title:'JavaScript: The Good Part', page: 101},
+        {title: 'Harry Potter', page: 39},
+        {title:'The Dark Tower', page: 85},
+        {title: 'Eloquent Ruby', page: 1}
+    ]
+}
